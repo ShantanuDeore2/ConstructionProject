7f5c631569e8c5f08b3561313848d0394b53947f
shantanu <shantanu.deore223@gmail.com>
2024-02-11 10:29:30 -0500

    Added Inventory schema

---
+++ schemas/Inventory.js
@@ -0,0 +1,31 @@
+// Description: Schema for Inventory.
+const mongoose = require("mongoose");
+const mongooseDelete = require("mongoose-delete");
+const applyLoggingHooks = require("./Hooks/loggingHook");
+const purchaseOrderMaterialSchema = require("./PurchaseOrderMaterial"); // Import the schema
+
+/**
+ * Inventory Schema - Represents an inventory with a material detail
+ * @property {mongoose.Schema} materialDetail - The material detail
+ * @type {mongoose.Schema}
+ */
+const inventorySchema = new mongoose.Schema(
+  {
+    // Embed the purchaseOrderMaterialSchema directly
+    materialDetail: purchaseOrderMaterialSchema,
+  },
+  { timestamps: true }
+);
+
+// Apply logging hooks to the schema
+applyLoggingHooks(inventorySchema);
+
+// Apply the mongoose-delete plugin
+inventorySchema.plugin(mongooseDelete, {
+  overrideMethods: "all",
+  deletedAt: true,
+});
+
+// TODO: add middleware for error handling
+
+module.exports = mongoose.model("Inventory", inventorySchema);
