e1459dcd0fd1ef3fd9ae402a09a718f019a2e0d0
shantanu <shantanu.deore223@gmail.com>
2024-02-17 23:55:22 -0500

    Added Dao for database abstraction

---
+++ api/dao/DAO.js
@@ -0,0 +1,8 @@
+// IRepository.js
+module.exports = class DAO {
+  async create(data) {}
+  async findAll() {}
+  async findById(id) {}
+  async updateById(id, data) {}
+  async deleteById(id) {}
+};
---
+++ api/dao/MongoDAO.js
@@ -0,0 +1,29 @@
+// MongoRepository.js
+const DAO = require("./DAO");
+
+module.exports = class MongoDAO extends DAO {
+  constructor(model) {
+    super();
+    this.model = model;
+  }
+
+  async create(document) {
+    return this.model.create(document);
+  }
+
+  async findAll(query = {}) {
+    return this.model.find(query);
+  }
+
+  async findById(id) {
+    return this.model.findById(id);
+  }
+
+  async updateById(id, update, options = { new: true, runValidators: true }) {
+    return this.model.findByIdAndUpdate(id, update, options);
+  }
+
+  async deleteById(id) {
+    return this.model.findByIdAndDelete(id);
+  }
+};
